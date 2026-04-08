f296bc9f320a9eec0e04f58606eac827470a6b9f
Ashish Gujjar <ashishgujjar757g@gmail.com>
2026-04-08 21:50:05 +0530

    Add save-money SVG icon to assets/images directory

---
+++ src/constants/filters.js
@@ -0,0 +1,45 @@
+export const timeFilters = [{
+    id:"this_week",
+    label:"This Week"
+},
+{
+    id:"this_year",
+    label:"This Year"
+},
+{
+    id:"this_month",
+    label:"This Month"
+},
+{
+    id:"today",
+    label:"Today"
+},
+{
+    id:"last_30_days",
+    label:"Last 30 Days"
+},
+{
+    id:"custom_range",
+    label:"Custom Range"
+}
+    
+]
+
+export const sortFilters = [
+{
+    id:"newest",
+    label:"Newest"
+},
+{
+    id:"oldest",
+    label:"Oldest"
+},
+{
+    id:"highest_amount",
+    label:"Highest Amount"
+},
+{
+    id:"lowest_amount",
+    label:"Lowest Amount"
+}
+]
