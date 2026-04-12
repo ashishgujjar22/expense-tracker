ccd5e4312b018e10d4b6482a4bd5e55673f4f486
Ashish Gujjar <ashishgujjar757g@gmail.com>
2026-04-12 20:45:48 +0530

    modify transaction

--- src/modules/transaction.js
+++ src/modules/transaction.js
@@ -22,7 +22,7 @@ export function addTransaction({ amount, description, category, date }) {
   //   console.log(data);
 }
 export function getTransactions() {
-   const g = hgjhfkk;
+   const g = hgjhfkkhjsdhjsgfsdfgfgs;
 }
 export function deleteTransaction(id) {
     
