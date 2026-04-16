4197196be2e30a7ce59fc39f8d482d41f873e6ee
Ashish Gujjar <ashishgujjar757g@gmail.com>
2026-04-16 20:56:09 +0530

    just add

--- src/modules/transaction.js
+++ src/modules/transaction.js
@@ -22,7 +22,7 @@ export function addTransaction({ amount, description, category, date }) {
   //   console.log(data);
 }
 export function getTransactions() {
-   const g = hgjhfkkhjsdhjsgfsdfgfgs;
+   const g = hgjhfkkhjsdhjsgcdfddfsdfgfgs;
 }
 export function deleteTransaction(id) {
     
